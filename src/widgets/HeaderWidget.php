7c8ec847a9f2d65a237d8d2cfd57308cc60ca875
Craig Fleming <rythos42@gmail.com>
2017-03-09 22:37:59 -0800

    Icon fonts for header buttons

--- src/widgets/HeaderWidget.php
+++ src/widgets/HeaderWidget.php
@@ -3,14 +3,18 @@ class HeaderWidget implements IWidget {
     public function render() {
         ?>
         <div id="Header">
-            <!-- ko with: logoutViewModel -->
-            <input id="Logout" type="button" value="<?php echo Translation::getString("logout"); ?>" data-bind="click: logout, visible: showLogout" class="ui-button ui-widget ui-corner-all" />
-            <!-- /ko -->
             <!-- ko with: createCampaignViewModel-->
             <input type="button" data-bind="click: requestCreateCampaign, visible: showCreateCampaignButton" value="<?php echo Translation::getString("createCampaign"); ?>" class="ui-button ui-widget ui-corner-all" />
             <!-- /ko -->
             <!-- ko with: userProfileViewModel -->
-            <input type="button" data-bind="click: requestUserProfile, visible: showUserProfileButton" value="<?php echo Translation::getString("profile"); ?>" class="ui-button ui-widget ui-corner-all" />
+            <button class="ui-button ui-widget ui-corner-all" data-bind="click: requestUserProfile, visible: showUserProfileButton" title="<?php echo Translation::getString("profile"); ?>">
+                <span class="icon-user"></span>
+            </button>
+            <!-- /ko -->
+            <!-- ko with: logoutViewModel -->
+            <button class="ui-button ui-widget ui-corner-all" data-bind="click: logout, visible: showLogout" title="<?php echo Translation::getString("logout"); ?>">
+                <span class="icon-exit"></span>
+            </button>
             <!-- /ko -->
         </div>
         <?php
