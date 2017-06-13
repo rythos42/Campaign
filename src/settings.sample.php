a9a0205550598c3078ae067f5d2f6cad0a67d10e
rythos42 <Blyrble7>
2017-06-13 09:57:58 -0700

    Adding sample settings

--- src/settings.sample.php
+++ src/settings.sample.php
@@ -5,6 +5,7 @@ $settings['databaseUsername'] = 'ENTER YOUR DATABASE USERNAME';
 $settings['databasePassword'] = 'ENTER YOUR DATABASE PASSWORD'; 
 $settings['databaseName'] = 'Campaign'; 
 $settings['installDirOnWebServer'] = '';
+$settings['siteSubDir'] = '';
 $settings['oneSignalAppId'] = null;
 $settings['oneSignalRestApiKey'] = null;
 ?>
